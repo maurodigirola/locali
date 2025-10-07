91c5c0e150e00cc313439a0c7e1b46e003410910
maurodigirola <maurodigirola@gmail.com>
2025-10-07 11:33:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "locali-firenze-v1";
 const ASSETS = [
   "./",
-  "./index.html",
+  "./Locali_Firenze_04.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
@@ -11,6 +11,15 @@ const ASSETS = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// attiva e cancella cache vecchie
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
 });
 
